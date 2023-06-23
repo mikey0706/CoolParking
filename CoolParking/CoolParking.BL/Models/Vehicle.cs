74300d67e7e77353b194cf9a9d81fe2a8048a603
mikey0706 <podenkomikhail@gmail.com>
2023-06-23 12:22:16 +0300

    GenerateRandomRegistrationPlateNumber being added to Vehicle

--- CoolParking/CoolParking.BL/Models/Vehicle.cs
+++ CoolParking/CoolParking.BL/Models/Vehicle.cs
@@ -5,6 +5,7 @@
 //       The Balance should be able to change only in the CoolParking.BL project.
 //       The type of constructor is shown in the tests and the constructor should have a validation, which also is clear from the tests.
 //       Static method GenerateRandomRegistrationPlateNumber should return a randomly generated unique identifier.
+using Fare;
 using System;
 using System.Text.RegularExpressions;
 
@@ -12,13 +13,14 @@ namespace CoolParking.BL.Models
 {
     public class Vehicle
     {
-        public string Id { get; set; }
-        public VehicleType Type { get; set; }
+        public readonly string Id;
+        public readonly VehicleType Type;
         public decimal Balance { get; set; }
+        private static readonly string _expression = @"[\w]([A-Z1-2])-\d{4}-[\w]([A-Z1-2])";
 
         public Vehicle(string id, VehicleType type, decimal balance)
         {
-            Regex regex = new Regex(@"[\w]([A-Z1-2])-\d{4}-[\w]([A-Z1-2])");
+            Regex regex = new Regex(_expression);
             if (!regex.IsMatch(id) || balance < 0)
             {
                 throw new ArgumentException();
@@ -26,6 +28,12 @@ namespace CoolParking.BL.Models
             Id = id;
             Type = type;
             Balance = balance;
+        }
+
+        public static string GenerateRandomRegistrationPlateNumber() 
+        {
+        Xeger xEger = new Xeger(_expression);
+        return xEger.Generate();
         }
 
     }
